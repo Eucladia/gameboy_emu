bf671b4166f871a9b367ba7adff75c69aa20ad0f
Eucladia <eucladialyra@gmail.com>
2025-02-09 17:24:59 -0500

    refactor(cpu): make `parse_instruction` return a guaranteed Instruction
    This function no longer needs to be optional because we panic if we got an unexpected byte.

--- src/cpu.rs
+++ src/cpu.rs
@@ -43,7 +43,7 @@ impl Cpu {
     self.registers.pc += 1;
   }
 
-  pub fn parse_instruction(&mut self, mmu: &Mmu) -> Option<Instruction> {
+  pub fn parse_instruction(&mut self, mmu: &Mmu) -> Instruction {
     let byte = mmu.read_byte(self.registers.pc);
 
     match byte {
@@ -52,10 +52,7 @@ impl Cpu {
         let dest_reg = Register::from_bits((byte >> 3) & 0b111).unwrap();
         let src_reg = Register::from_bits(byte & 0b111).unwrap();
 
-        Some(Instruction::LD(
-          Operand::Register(dest_reg),
-          Operand::Register(src_reg),
-        ))
+        Instruction::LD(Operand::Register(dest_reg), Operand::Register(src_reg))
       }
       // LD r16, n16
       0x01 | 0x11 | 0x21 | 0x31 => {
@@ -70,10 +67,7 @@ impl Cpu {
 
         self.registers.pc += 2;
 
-        Some(Instruction::LD(
-          Operand::RegisterPair(dest_reg_pair),
-          Operand::Word(n16),
-        ))
+        Instruction::LD(Operand::RegisterPair(dest_reg_pair), Operand::Word(n16))
       }
       // LD [r16], A
       0x02 | 0x12 => {
@@ -83,10 +77,10 @@ impl Cpu {
           RegisterPair::DE
         };
 
-        Some(Instruction::LD(
+        Instruction::LD(
           Operand::RegisterPairMemory(dest_reg_pair),
           Operand::Register(Register::A),
-        ))
+        )
       }
       // LD A, [r16]
       0x0A | 0x1A => {
@@ -96,10 +90,10 @@ impl Cpu {
           RegisterPair::DE
         };
 
-        Some(Instruction::LD(
+        Instruction::LD(
           Operand::Register(Register::A),
           Operand::RegisterPairMemory(src_reg_pair),
-        ))
+        )
       }
       // LD r8 | [HL], n8
       0x06 | 0x16 | 0x26 | 0x36 | 0x0E | 0x1E | 0x2E | 0x3E => {
@@ -108,10 +102,7 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::LD(
-          Operand::Register(dest_reg),
-          Operand::Byte(n8),
-        ))
+        Instruction::LD(Operand::Register(dest_reg), Operand::Byte(n8))
       }
       // LD HL, SP + n8
       0xF8 => {
@@ -120,16 +111,16 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::LD(
+        Instruction::LD(
           Operand::RegisterPair(RegisterPair::HL),
           Operand::Word(self.registers.sp.wrapping_add(n8 as u16)),
-        ))
+        )
       }
       // LD SP, HL
-      0xF9 => Some(Instruction::LD(
+      0xF9 => Instruction::LD(
         Operand::RegisterPair(RegisterPair::SP),
         Operand::RegisterPair(RegisterPair::HL),
-      )),
+      ),
 
       // LD [n16], A
       0xEA => {
@@ -137,10 +128,7 @@ impl Cpu {
 
         self.registers.pc += 2;
 
-        Some(Instruction::LD(
-          Operand::MemoryAddress(n16),
-          Operand::Register(Register::A),
-        ))
+        Instruction::LD(Operand::MemoryAddress(n16), Operand::Register(Register::A))
       }
       // LD A, [n16]
       0xFA => {
@@ -148,49 +136,43 @@ impl Cpu {
 
         self.registers.pc += 2;
 
-        Some(Instruction::LD(
-          Operand::Register(Register::A),
-          Operand::MemoryAddress(n16),
-        ))
+        Instruction::LD(Operand::Register(Register::A), Operand::MemoryAddress(n16))
       }
       // LDI [HL], A
-      0x22 => Some(Instruction::LDI(
+      0x22 => Instruction::LDI(
         Operand::RegisterPairMemory(RegisterPair::HL),
         Operand::Register(Register::A),
-      )),
+      ),
       // LDI A, [HL]
-      0x2A => Some(Instruction::LDI(
+      0x2A => Instruction::LDI(
         Operand::Register(Register::A),
         Operand::RegisterPairMemory(RegisterPair::HL),
-      )),
+      ),
       // LDD [HL], A
-      0x32 => Some(Instruction::LDD(
+      0x32 => Instruction::LDD(
         Operand::RegisterPairMemory(RegisterPair::HL),
         Operand::Register(Register::A),
-      )),
+      ),
       // LDD A, [HL]
-      0x3A => Some(Instruction::LDD(
+      0x3A => Instruction::LDD(
         Operand::Register(Register::A),
         Operand::RegisterPairMemory(RegisterPair::HL),
-      )),
+      ),
       // LDH [0xFF00 + C], A
-      0xE2 => Some(Instruction::LDH(
+      0xE2 => Instruction::LDH(
         Operand::RegisterMemory(Register::C),
         Operand::Register(Register::A),
-      )),
+      ),
       // LDH A, [0xFF00 + C]
-      0xF2 => Some(Instruction::LDH(
+      0xF2 => Instruction::LDH(
         Operand::Register(Register::A),
         Operand::RegisterMemory(Register::C),
-      )),
+      ),
       // ADC A, r8 | [HL]
       0x88..=0x8F => {
         let src_reg = Register::from_bits((byte & 0b111)).unwrap();
 
-        Some(Instruction::ADC(
-          Operand::Register(Register::A),
-          Operand::Register(src_reg),
-        ))
+        Instruction::ADC(Operand::Register(Register::A), Operand::Register(src_reg))
       }
       // ADC A, n8
       0xCE => {
@@ -198,19 +180,13 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::ADC(
-          Operand::Register(Register::A),
-          Operand::Byte(n8),
-        ))
+        Instruction::ADC(Operand::Register(Register::A), Operand::Byte(n8))
       }
       // ADD A, r8 | [HL]
       0x80..=0x87 => {
         let src_reg = Register::from_bits((byte & 0b111)).unwrap();
 
-        Some(Instruction::ADD(
-          Operand::Register(Register::A),
-          Operand::Register(src_reg),
-        ))
+        Instruction::ADD(Operand::Register(Register::A), Operand::Register(src_reg))
       }
       // ADD A, n8
       0xC6 => {
@@ -218,10 +194,7 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::ADD(
-          Operand::Register(Register::A),
-          Operand::Byte(n8),
-        ))
+        Instruction::ADD(Operand::Register(Register::A), Operand::Byte(n8))
       }
       // ADD SP, n8
       0xE8 => {
@@ -229,19 +202,13 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::ADD(
-          Operand::RegisterPair(RegisterPair::SP),
-          Operand::Byte(n8),
-        ))
+        Instruction::ADD(Operand::RegisterPair(RegisterPair::SP), Operand::Byte(n8))
       }
       // AND A, r8 | [HL]
       0xA0..=0xA7 => {
         let src_reg = Register::from_bits(byte & 0b111).unwrap();
 
-        Some(Instruction::AND(
-          Operand::Register(Register::A),
-          Operand::Register(src_reg),
-        ))
+        Instruction::AND(Operand::Register(Register::A), Operand::Register(src_reg))
       }
       // AND A, n8
       0xE6 => {
@@ -249,19 +216,13 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::AND(
-          Operand::Register(Register::A),
-          Operand::Byte(n8),
-        ))
+        Instruction::AND(Operand::Register(Register::A), Operand::Byte(n8))
       }
       // CP A, r8 | [HL]
       0xB8..=0xBF => {
         let src_reg = Register::from_bits(byte & 0b111).unwrap();
 
-        Some(Instruction::CP(
-          Operand::Register(Register::A),
-          Operand::Register(src_reg),
-        ))
+        Instruction::CP(Operand::Register(Register::A), Operand::Register(src_reg))
       }
       // CP A, n8
       0xFE => {
@@ -269,19 +230,13 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::CP(
-          Operand::Register(Register::A),
-          Operand::Byte(n8),
-        ))
+        Instruction::CP(Operand::Register(Register::A), Operand::Byte(n8))
       }
       // OR A, r8 | [HL]
       0xB0..=0xB7 => {
         let src_reg = Register::from_bits(byte & 0b111).unwrap();
 
-        Some(Instruction::OR(
-          Operand::Register(Register::A),
-          Operand::Register(src_reg),
-        ))
+        Instruction::OR(Operand::Register(Register::A), Operand::Register(src_reg))
       }
       // OR A, n8
       0xF6 => {
@@ -289,19 +244,13 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::OR(
-          Operand::Register(Register::A),
-          Operand::Byte(n8),
-        ))
+        Instruction::OR(Operand::Register(Register::A), Operand::Byte(n8))
       }
       // SBC A, r8 | [HL]
       0x98..=0x9F => {
         let src_reg = Register::from_bits(byte & 0b111).unwrap();
 
-        Some(Instruction::SBC(
-          Operand::Register(Register::A),
-          Operand::Register(src_reg),
-        ))
+        Instruction::SBC(Operand::Register(Register::A), Operand::Register(src_reg))
       }
       // SBC A, n8
       0xDE => {
@@ -309,19 +258,13 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::SBC(
-          Operand::Register(Register::A),
-          Operand::Byte(n8),
-        ))
+        Instruction::SBC(Operand::Register(Register::A), Operand::Byte(n8))
       }
       // SUB A, r8 | [HL]
       0x90..=0x97 => {
         let src_reg = Register::from_bits(byte & 0b111).unwrap();
 
-        Some(Instruction::SUB(
-          Operand::Register(Register::A),
-          Operand::Register(src_reg),
-        ))
+        Instruction::SUB(Operand::Register(Register::A), Operand::Register(src_reg))
       }
       // SUB A, n8
       0xD6 => {
@@ -329,19 +272,13 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::SUB(
-          Operand::Register(Register::A),
-          Operand::Byte(n8),
-        ))
+        Instruction::SUB(Operand::Register(Register::A), Operand::Byte(n8))
       }
       // XOR A, r8 | [HL]
       0xA8..=0xAF => {
         let src_reg = Register::from_bits(byte & 0b111).unwrap();
 
-        Some(Instruction::XOR(
-          Operand::Register(Register::A),
-          Operand::Register(src_reg),
-        ))
+        Instruction::XOR(Operand::Register(Register::A), Operand::Register(src_reg))
       }
       // XOR A, n8
       0xEE => {
@@ -349,13 +286,10 @@ impl Cpu {
 
         self.registers.pc += 1;
 
-        Some(Instruction::XOR(
-          Operand::Register(Register::A),
-          Operand::Byte(n8),
-        ))
+        Instruction::XOR(Operand::Register(Register::A), Operand::Byte(n8))
       }
       // DAA
-      0x27 => Some(Instruction::DAA),
+      0x27 => Instruction::DAA,
 
       byte => panic!("unimplemented: {byte} ({byte:02X})"),
     }
