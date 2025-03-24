8d0e77503755ed93942bc3f03715b400944335d3
Eucladia <eucladialyra@gmail.com>
2025-03-23 22:17:19 -0400

    feat: FPS counter

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod instructions;
 mod interrupts;
 
 use emulator::Emulator;
+use flags::is_flag_set;
 use hardware::{Cpu, Hardware, joypad::Button};
 
 use softbuffer::{Context, Surface};
@@ -63,6 +64,13 @@ fn main() {
   let mut last_width = INITIAL_GAMEBOY_WIDTH;
   let mut last_height = INITIAL_GAMEBOY_HEIGHT;
 
+  let mut show_fps = false;
+  let mut fps = 0.0;
+  let mut num_frames = 0;
+  let mut last_fps_update = last_update;
+
+  let mut is_shift_held = false;
+
   let mut window_frame = vec![0; (last_width * last_height) as usize];
 
   event_loop
@@ -98,10 +106,16 @@ fn main() {
                 },
               ..
             },
-        } if window_id == window.id() => {
-          if matches!(physical_key, PhysicalKey::Code(KeyCode::Space))
-            && matches!(state, ElementState::Released)
+        } if window_id == window.id() => match physical_key {
+          PhysicalKey::Code(KeyCode::ShiftLeft | KeyCode::ShiftRight) => {
+            is_shift_held = matches!(state, ElementState::Pressed);
+          }
+          PhysicalKey::Code(KeyCode::Digit1)
+            if is_shift_held && matches!(state, ElementState::Pressed) =>
           {
+            show_fps = !show_fps;
+          }
+          PhysicalKey::Code(KeyCode::Space) if matches!(state, ElementState::Released) => {
             limit_frames = !limit_frames;
 
             if limit_frames {
@@ -111,12 +125,15 @@ fn main() {
             }
 
             window.request_redraw();
-          } else if let Some(gb_button) = convert_button(&physical_key) {
-            emulator
-              .hardware
-              .update_button(gb_button, matches!(state, ElementState::Pressed))
           }
-        }
+          key => {
+            if let Some(gb_button) = convert_button(&key) {
+              emulator
+                .hardware
+                .update_button(gb_button, matches!(state, ElementState::Pressed))
+            }
+          }
+        },
 
         Event::WindowEvent {
           window_id,
@@ -140,6 +157,7 @@ fn main() {
                 .unwrap();
 
               window_frame.resize((width * height) as usize, 0);
+
               last_width = width;
               last_height = height;
             }
@@ -181,6 +199,34 @@ fn main() {
               }
             }
 
+            num_frames += 1;
+
+            let delta = now.duration_since(last_fps_update).as_secs_f64();
+
+            if delta >= 1.0 {
+              fps = num_frames as f64 / delta;
+              last_fps_update = now;
+              num_frames = 0;
+            }
+
+            if show_fps {
+              const FPS_X_POS: u32 = 2;
+              const FPS_Y_POS: u32 = 2;
+              const FPS_TEXT_COLOR: u32 = 0x00FF0000;
+
+              let fps_text = format!("FPS: {:.4}", fps);
+
+              draw_text(
+                &fps_text,
+                &mut window_frame,
+                width,
+                FPS_X_POS,
+                FPS_Y_POS,
+                FPS_TEXT_COLOR,
+                scale as u32,
+              );
+            }
+
             let mut buffer = surface.buffer_mut().unwrap();
 
             buffer.copy_from_slice(&window_frame);
@@ -197,12 +243,215 @@ fn main() {
     .unwrap();
 }
 
-/// Computes the scale factor for the game.
-fn compute_scale_factor(window_width: u32, window_height: u32) -> f64 {
-  let scale_x = window_width as f64 / GAMEBOY_WIDTH as f64;
-  let scale_y = window_height as f64 / GAMEBOY_HEIGHT as f64;
+/// Draws the text into the buffer at the following x and y position.
+fn draw_text(
+  text: &str,
+  buffer: &mut [u32],
+  buffer_width: u32,
+  x_pos: u32,
+  y_pos: u32,
+  color: u32,
+  scale: u32,
+) {
+  for (character_x_pos, bitmap) in text.as_bytes().iter().enumerate().flat_map(|(index, b)| {
+    get_character_bitmap(*b).map(|row| {
+      (
+        x_pos + (index as u32 * DEFAULT_CHARACTER_WIDTH * scale),
+        row,
+      )
+    })
+  }) {
+    for (row, bits) in bitmap.iter().enumerate() {
+      for col in 0..DEFAULT_CHARACTER_WIDTH {
+        let mask = 1 << (DEFAULT_CHARACTER_WIDTH - 1 - col);
 
-  scale_x.min(scale_y).max(1.0)
+        if is_flag_set!(bits, mask) {
+          for dx in 0..scale {
+            for dy in 0..scale {
+              let draw_x = character_x_pos + col * scale + dx;
+              let draw_y = y_pos + row as u32 * scale + dy;
+              let buffer_index = draw_y * buffer_width + draw_x;
+
+              if buffer_index < buffer.len() as u32 {
+                buffer[buffer_index as usize] = color;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+const DEFAULT_CHARACTER_WIDTH: u32 = 7;
+const DEFAULT_CHARACTER_HEIGHT: u32 = 8;
+
+/// Converts the ASCII byte to a 7x8 bitmap.
+#[rustfmt::skip]
+const fn get_character_bitmap(byte: u8) -> Option<[u8; DEFAULT_CHARACTER_HEIGHT as usize]> {
+  match byte {
+    b'0' => Some([
+      0b0111100,
+      0b1000010,
+      0b1000010,
+      0b1000010,
+      0b1000010,
+      0b1000010,
+      0b1000010,
+      0b0111100,
+    ]),
+    b'1' => Some([
+      0b0011000,
+      0b0101000,
+      0b0001000,
+      0b0001000,
+      0b0001000,
+      0b0001000,
+      0b0001000,
+      0b0111100,
+    ]),
+    b'2' => Some([
+      0b0111100,
+      0b1000010,
+      0b0000010,
+      0b0000100,
+      0b0001000,
+      0b0010000,
+      0b0100000,
+      0b1111110,
+    ]),
+    b'3' => Some([
+      0b0111100,
+      0b1000010,
+      0b0000010,
+      0b0011100,
+      0b0000010,
+      0b0000010,
+      0b1000010,
+      0b0111100,
+    ]),
+    b'4' => Some([
+      0b0000100,
+      0b0001100,
+      0b0010100,
+      0b0100100,
+      0b1000100,
+      0b1111110,
+      0b0000100,
+      0b0000100,
+    ]),
+    b'5' => Some([
+      0b1111110,
+      0b1000000,
+      0b1000000,
+      0b1111100,
+      0b0000010,
+      0b0000010,
+      0b1000010,
+      0b0111100,
+    ]),
+    b'6' => Some([
+      0b0111100,
+      0b1000010,
+      0b1000000,
+      0b1111100,
+      0b1000010,
+      0b1000010,
+      0b1000010,
+      0b0111100,
+    ]),
+    b'7' => Some([
+      0b1111110,
+      0b0000010,
+      0b0000100,
+      0b0001000,
+      0b0010000,
+      0b0010000,
+      0b0010000,
+      0b0010000,
+    ]),
+    b'8' => Some([
+      0b0111100,
+      0b1000010,
+      0b1000010,
+      0b0111100,
+      0b1000010,
+      0b1000010,
+      0b1000010,
+      0b0111100,
+    ]),
+    b'9' => Some([
+      0b0111100,
+      0b1000010,
+      0b1000010,
+      0b0111110,
+      0b0000010,
+      0b0000010,
+      0b1000010,
+      0b0111100,
+    ]),
+    b'F' => Some([
+      0b1111110,
+      0b1000000,
+      0b1000000,
+      0b1111100,
+      0b1000000,
+      0b1000000,
+      0b1000000,
+      0b1000000,
+    ]),
+    b'P' => Some([
+      0b1111100,
+      0b1000010,
+      0b1000010,
+      0b1111100,
+      0b1000000,
+      0b1000000,
+      0b1000000,
+      0b1000000,
+    ]),
+    b'S' => Some([
+      0b0111100,
+      0b1000010,
+      0b1000000,
+      0b0111100,
+      0b0000010,
+      0b0000010,
+      0b1000010,
+      0b0111100,
+    ]),
+    b':' => Some([
+      0b0000000,
+      0b0011000,
+      0b0011000,
+      0b0000000,
+      0b0000000,
+      0b0011000,
+      0b0011000,
+      0b0000000,
+    ]),
+    b'.' => Some([
+      0b0000000,
+      0b0000000,
+      0b0000000,
+      0b0000000,
+      0b0000000,
+      0b0000000,
+      0b0011000,
+      0b0011000,
+    ]),
+    b' ' => Some([
+      0b00000000,
+      0b00000000,
+      0b00000000,
+      0b00000000,
+      0b00000000,
+      0b00000000,
+      0b00000000,
+      0b00000000,
+    ]),
+    _ => None,
+  }
 }
 
 /// Converts a winit key into a Gameboy button.
@@ -221,4 +470,12 @@ fn convert_button(physical_key: &PhysicalKey) -> Option<Button> {
 
     _ => return None,
   })
+}
+
+/// Computes the scale factor for the game.
+fn compute_scale_factor(window_width: u32, window_height: u32) -> f64 {
+  let scale_x = window_width as f64 / GAMEBOY_WIDTH as f64;
+  let scale_y = window_height as f64 / GAMEBOY_HEIGHT as f64;
+
+  scale_x.min(scale_y).max(1.0)
 }
